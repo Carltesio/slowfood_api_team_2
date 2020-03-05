d581b35f3b0d7bbe3cffcde758f4057cc47d508a
Hunter <hmvitous@yahoo.com>
2020-03-05 11:04:48 +0100

    Creates a Product model to hold the products info

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Product has db column'
+    it {is_expected.to have_db_column :id}
+    it {is_expected.to have_db_column :name}
+    it {is_expected.to have_db_column :price}
+  end
+  
+  describe 'Validations' do
+    it {is_expected.to validate_presence_of :name}
+    it {is_expected.to validate_presence_of :price}
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:product)).to be_valid
+    end 
+  end
+
+end
+
+
+
